9738e9781ede50b4754ba69c563cd911364288c0
Dawae111 <DavidApply2022@outlook.com>
2023-09-16 18:59:59 -0400

    Initial Commit

---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by David on 2023/9/16.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
