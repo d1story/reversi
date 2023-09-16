0b290ebed2f07220b7184c2f37b77db4bf94d3bd
d1story <ericl7552@gmail.com>
2023-09-16 19:28:34 -0400

    Initial Commit

---
+++ reversi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reversi
+//
+//  Created by eric liu on 2023-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
