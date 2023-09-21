349f6829115c8d54d2928e754652ab0fff4e6b58
Dawae111 <DavidApply2022@outlook.com>
2023-09-21 15:01:30 -0400

    saving

---
+++ Reversi/background.swift
@@ -0,0 +1,20 @@
+//
+//  background.swift
+//  Reversi
+//
+//  Created by David on 2023/9/17.
+//
+
+import SwiftUI
+
+struct background: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct background_Previews: PreviewProvider {
+    static var previews: some View {
+        background()
+    }
+}
---
+++ Reversi/playeringame2.swift
@@ -0,0 +1,18 @@
+//
+//  playeringame2.swift
+//  Reversi
+//
+//  Created by David on 2023/9/21.
+//
+
+import SwiftUI
+
+struct playeringame2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    playeringame2()
+}
